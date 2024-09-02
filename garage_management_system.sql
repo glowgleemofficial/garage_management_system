dc4097a8b6619b34d7452b2e6c1e1e2bc3c7ea6c
majidali343 <majidali37406@gmail.com>
2024-09-02 17:48:10 +0500

    datbase date changed

--- garage_management_system.sql
+++ garage_management_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 01, 2024 at 03:59 PM
+-- Generation Time: Sep 02, 2024 at 02:46 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -32,7 +32,7 @@ CREATE TABLE `customers` (
   `name` varchar(30) NOT NULL,
   `vehicle` varchar(50) NOT NULL,
   `description` varchar(100) NOT NULL,
-  `date` date NOT NULL,
+  `date` varchar(255) NOT NULL,
   `contact` int(25) NOT NULL,
   `deals` varchar(20) NOT NULL,
   `customer` varchar(10) NOT NULL
@@ -48,10 +48,18 @@ CREATE TABLE `employeesalaary` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `job_title` varchar(255) NOT NULL,
-  `date` date NOT NULL,
+  `date` varchar(255) NOT NULL,
   `salary` decimal(10,2) NOT NULL,
   `salary_status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employeesalaary`
+--
+
+INSERT INTO `employeesalaary` (`id`, `name`, `job_title`, `date`, `salary`, `salary_status`) VALUES
+(1, 'asdasd', 'Mechanic', '2024-09-10', 34234.00, 'Paid'),
+(2, 'asd', 'Boom loader operator', '2024-09-04', 123123.00, 'Paid');
 
 -- --------------------------------------------------------
 
@@ -66,7 +74,7 @@ CREATE TABLE `employee_task` (
   `location` varchar(50) NOT NULL,
   `task` varchar(50) NOT NULL,
   `work_hours` int(10) NOT NULL,
-  `date` date NOT NULL,
+  `date` varchar(255) NOT NULL,
   `time` time(6) NOT NULL,
   `task_status` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -82,10 +90,19 @@ CREATE TABLE `expenses` (
   `name` varchar(30) NOT NULL,
   `vehicle` varchar(50) NOT NULL,
   `description` varchar(100) NOT NULL,
-  `date` date NOT NULL,
+  `date` varchar(255) NOT NULL,
   `amount` int(50) NOT NULL,
   `payment_status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `expenses`
+--
+
+INSERT INTO `expenses` (`id`, `name`, `vehicle`, `description`, `date`, `amount`, `payment_status`) VALUES
+(2, 'sdsd', 'Crane: 50-Ton', 'qweqwew', '2024-09-01T12:36:56.000Z', 344, 'Online'),
+(3, 'sdsd', 'Crane: 50-Ton', 'qweqwew', '2024-09-01T12:36:56.000Z', 344, 'Online'),
+(4, 'hhh', 'Boomloader: 540', 'llllllsd', '2024-09-12T12:39:03.000Z', 234234, 'Online');
 
 -- --------------------------------------------------------
 
@@ -97,7 +114,7 @@ CREATE TABLE `income` (
   `id` int(11) NOT NULL,
   `name` varchar(11) NOT NULL,
   `description` varchar(100) NOT NULL,
-  `date` date NOT NULL,
+  `date` varchar(255) NOT NULL,
   `salary` int(50) NOT NULL,
   `salary_status` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -114,7 +131,7 @@ CREATE TABLE `invoice` (
   `vehicle` varchar(50) NOT NULL,
   `description` varchar(50) NOT NULL,
   `Location` varchar(255) NOT NULL,
-  `date` date NOT NULL,
+  `date` varchar(255) NOT NULL,
   `time` time(6) NOT NULL,
   `advance` int(50) NOT NULL,
   `pending` int(50) NOT NULL,
@@ -131,7 +148,7 @@ CREATE TABLE `pending` (
   `id` int(30) NOT NULL,
   `name` varchar(30) NOT NULL,
   `location` varchar(50) NOT NULL,
-  `date` date NOT NULL,
+  `date` varchar(255) NOT NULL,
   `advance` int(50) NOT NULL,
   `pending` int(50) NOT NULL,
   `total` int(50) NOT NULL,
@@ -149,7 +166,7 @@ CREATE TABLE `vehicles` (
   `name` varchar(30) NOT NULL,
   `vehicle` varchar(50) NOT NULL,
   `description` varchar(100) NOT NULL,
-  `date` date NOT NULL,
+  `date` varchar(255) NOT NULL,
   `location` varchar(50) NOT NULL,
   `charges` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
