fbaed9ccbe0ca2bf6712117796312bdaf0a74391
glowgleemofficial <glowgleemofficial@gmail.com>
2024-09-18 20:11:07 +0500

    Update db.js

--- BackEnd/Config/db.js
+++ BackEnd/Config/db.js
@@ -1,15 +1,13 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
 
-var connectDB = mysql.createConnection({
+// Creating a MySQL connection pool
+var pool = mysql.createPool({
+  connectionLimit: 10, // Adjust the number based on your needs
   host: "localhost",
   user: "root",
   password: 'P@kistan1444',
   database: 'Garage_Management_System'
 });
 
-connectDB.connect(function(err) {
-  if (err) throw err;
-  console.log("Database is Connected!");
-});
-
-module.exports= connectDB;
+// Exporting the pool instead of a single connection
+module.exports = pool;
